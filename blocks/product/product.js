ef992903ab3bc80a7b795c771c1960237aa779d6
demo-cqsvr <66979397+demo-cqsvr@users.noreply.github.com>
2025-11-18 11:54:38 +0900

    add release region class

--- blocks/product/product.js
+++ blocks/product/product.js
@@ -1,4 +1,7 @@
 export default function decorate(block) {
-  // 空の実装
-
+  // 最初のp要素にrelease_regionクラスを付与
+  const firstP = block.querySelector('div > div > p');
+  if (firstP) {
+    firstP.classList.add('release_region');
+  }
 }
